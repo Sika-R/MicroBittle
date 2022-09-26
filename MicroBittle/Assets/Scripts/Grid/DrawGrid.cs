99b7767604ff879eb5b8048959d874ae7625ede0
Yuchen Xie <franphegor@126.com>
2022-09-26 14:20:27 -0400

    Add grid test

---
+++ MicroBittle/Assets/Scripts/Grid/DrawGrid.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[ExecuteInEditMode]
+public class DrawGrid : MonoBehaviour
+{
+	[SerializeField]
+    bool bShowGizmos = true;
+
+    [SerializeField]
+    int m_column = 9; //列
+    [SerializeField]
+    int m_row = 9; //行
+ 
+ 	[SerializeField]
+    public float m_gridSize = 1.0f; //大概的大小
+    private readonly Color m_color = Color.white;
+
+    private BoxCollider m_collider; //用来做射线检测
+
+    [HideInInspector]
+    public GameObject m_obstaclePrefab;
+ 
+ 	
+ 	private void Start()
+    {
+        //gameObject.layer = 31;
+        m_collider = GetComponent<BoxCollider>();
+        if (m_collider == null)
+        {
+            m_collider = gameObject.AddComponent<BoxCollider>();
+            m_collider.size = new Vector3(100, 0.1f, 100);
+        }
+    }
+
+    private void GridGizmo(int cols, int rows)
+    {
+        Vector3 start;
+        Vector3 end;
+        for (int i = 0; i <= cols; i++)
+        {
+            start = new Vector3(i * m_gridSize - cols / 2 * m_gridSize, 0, -rows / 2 * m_gridSize);
+            end = new Vector3(i * m_gridSize - cols / 2 * m_gridSize, 0, rows / 2 * m_gridSize);
+            Gizmos.DrawLine(transform.position + start, transform.position + end);
+        }
+        for (int j = 0; j <= rows; j++)
+        {
+            start = new Vector3(- cols / 2 * m_gridSize, 0, -rows / 2 * m_gridSize + j * m_gridSize);
+            end = new Vector3(cols * m_gridSize - cols / 2 * m_gridSize, 0, -rows / 2 * m_gridSize + j * m_gridSize);
+            Gizmos.DrawLine(transform.position + start, transform.position + end);
+        }
+    }
+ 
+    private void OnDrawGizmos()
+    {
+    	if (bShowGizmos == false || Application.isEditor == false )
+    	{
+            return;
+    	}
+        Gizmos.color = m_color;
+        GridGizmo(m_column, m_row);
+    }
+
+    public Vector3 IdentifyCenter(Vector3 hit)
+    {
+        float x = hit.x - transform.position.x;
+        float z = hit.z - transform.position.z;
+    	if(x < -m_column * m_gridSize || x > m_column * m_gridSize / 2 || z < -m_row * m_gridSize / 2 || z > m_row * m_gridSize / 2)
+    	{
+    		return new Vector3(-1, -1, -1);
+    	}
+    	int xx = (int)Mathf.Floor(x / m_gridSize);
+    	int zz = (int)Mathf.Floor(z / m_gridSize);
+        Vector3 position = new Vector3((xx + 0.5f) * m_gridSize, 0, (zz + 0.5f) * m_gridSize);
+    	return position + transform.position;
+    }
+}
+
---
+++ MicroBittle/Assets/Scripts/Grid/GridInspectorLogic.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DrawGrid))]
+public class GridInspectorLogic : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DrawGrid gridObj = (DrawGrid)target;
+        gridObj.m_obstaclePrefab = EditorGUILayout.ObjectField("Choose Object to place",gridObj.m_obstaclePrefab,typeof(GameObject),true)as GameObject;
+
+    }
+
+    public void OnSceneGUI()
+    {
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        RaycastHit hitInfo;
+        bool isDown = false;
+        LayerMask mask = 1 << 9 | 1 << 10;
+        if (Physics.Raycast(ray, out hitInfo, 2000, ~mask)) //这里设置层
+        {
+        	if(hitInfo.collider.tag == "Floor")
+        	{
+        		float x = hitInfo.point.x;
+	            float y = hitInfo.point.y;
+	            Event e = Event.current;
+	            if (!isDown && e.type == EventType.KeyDown)
+	            {
+	                if (e.keyCode == KeyCode.LeftShift)
+	                {
+	                	isDown = true;
+	                	GameObject prefab = ((DrawGrid)target).m_obstaclePrefab;
+	                	Vector3 initPoint = ((DrawGrid)target).IdentifyCenter(hitInfo.point);
+	                	initPoint.y += prefab.GetComponent<BoxCollider>().size.y * prefab.transform.localScale.y / 2 * ((DrawGrid)target).m_gridSize;
+
+						GameObject newobj = Instantiate(prefab, initPoint, Quaternion.Euler(0, 0, 0), ((DrawGrid)target).transform) as GameObject;  //设置障碍 
+						// Debug.Log(newobj.transform.localScale);
+                		newobj.transform.localScale = ((DrawGrid)target).m_gridSize * newobj.transform.localScale;
+                		Undo.RegisterCreatedObjectUndo(newobj, "NewObject");
+	                }
+	            }
+        	}
+        	else if(hitInfo.collider.tag == "Cube")
+        	{
+				float x = hitInfo.point.x;
+	            float y = hitInfo.point.y;
+	            Event e = Event.current;
+	            if (!isDown && e.type == EventType.KeyDown)
+	            {
+	                if (e.keyCode == KeyCode.LeftControl)
+	                {
+	                	isDown = true;
+	                	Undo.DestroyObjectImmediate(hitInfo.transform.gameObject);
+	                }
+	                else if(e.keyCode == KeyCode.LeftShift)
+	                {
+	                	isDown = true;
+	                	Transform bottom = hitInfo.transform;
+	                	DrawGrid drawgrid = bottom.gameObject.GetComponentInParent<DrawGrid>();
+	                	GameObject prefab = drawgrid.m_obstaclePrefab;
+	                	Vector3 initPoint = bottom.position;
+	                	initPoint.y += bottom.gameObject.GetComponent<BoxCollider>().size.y * bottom.localScale.y / 2;
+	                	initPoint.y += prefab.GetComponent<BoxCollider>().size.y * prefab.transform.localScale.y / 2 * ((DrawGrid)target).m_gridSize;
+	                	GameObject newobj = Instantiate(prefab, initPoint, Quaternion.Euler(0, 0, 0), ((DrawGrid)target).transform) as GameObject;  //设置障碍 
+                		newobj.transform.localScale = newobj.transform.localScale = ((DrawGrid)target).m_gridSize * newobj.transform.localScale;
+                		Undo.RegisterCreatedObjectUndo(newobj, "NewObject");
+	                }
+	            }
+        	}
+            
+        }
+    }
+}
+
+
