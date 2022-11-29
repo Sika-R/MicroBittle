cbf98bd64033b0403a046c1f19612987bc2b45b7
MuruC <mc6251@nyu.edu>
2022-11-29 15:01:54 -0500

    dialogue

--- MicroBittle/Assets/Scripts/ChangeScene.cs
+++ MicroBittle/Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public string sceneName;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,6 @@ public class ChangeScene : MonoBehaviour
 
     public void ChangeSceneEvent()
     {
-        SceneManager.LoadScene("customize");
+        SceneManager.LoadScene(sceneName);
     }
 }
---
+++ MicroBittle/Assets/Scripts/Dialogue/DialogueControllerProgramFlow_StoryMode.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueControllerProgramFlow_StoryMode : DialogueController
+{
+    public GameObject DialogueUI;
+    public GameObject PowerLog;
+    public static DialogueControllerProgramFlow_StoryMode Instance_ = null;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance_ != null && Instance_ != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance_ = this;
+        }
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    // Update is called once per frame
+    public override void Update()
+    {
+        return;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public override void IncreaseDialogueIndex()
+    {
+        if (dialogueIndex == 0) {
+            PowerLog.SetActive(true);
+            StartCoroutine(doInteraction());
+        }
+        else if (dialogueIndex == 1)
+        {
+            PowerLog.SetActive(false);
+            DialogueUI.SetActive(false);
+        }
+        if (dialogueIndex == dialogues.Count - 1)
+        {
+            dialogueEndEvent.Invoke();
+        }
+        dialogueIndex++;
+    }
+
+    public void AfterFollowingInstruction()
+    {
+        DialogueUI.SetActive(true);
+        DoInteraction();
+    }
+
+    IEnumerator doInteraction()
+    {
+        yield return new WaitForSeconds(1);
+        DoInteraction();
+    }
+}
