0d7c4dcc483cd06996c0a7962970216f3d9b0853
MuruC <mc6251@nyu.edu>
2022-12-05 14:28:04 -0500

    update mute button

--- MicroBittle/Assets/Scripts/SoundMgr.cs
+++ MicroBittle/Assets/Scripts/SoundMgr.cs
@@ -127,6 +127,20 @@ public class SoundMgr : MonoBehaviour
         MuteAllAs();
     }
 
+    public void ClickMute()
+    {
+        if (PlayerPrefs.GetInt("IsMuted") == 0)
+        {
+            PlayerPrefs.SetInt("IsMuted", 1);
+            Debug.Log(PlayerPrefs.GetInt("IsMuted"));
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsMuted", 0);
+        }
+        MuteAllAs();
+    }
+
     public void MuteAllAs() {
         int isEnabledAllAudioSource = PlayerPrefs.GetInt("IsMuted");
         muteButton.sprite = muteSprite[isEnabledAllAudioSource];
